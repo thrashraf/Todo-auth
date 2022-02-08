c8c549e0081c278f9d4dc820cf78e6cec73ed57c
Ashraf <zulashrafvns@gmail.com>
2022-02-08 17:51:55 +0800

    fix

---
+++ auth-client/src/pages/Todos.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { UsersTable } from './UsersTable'
+   
+export const Todos = () => {
+    return (
+        <div>
+            <UsersTable seeTodos={true }/>
+        </div>
+    )
+}
